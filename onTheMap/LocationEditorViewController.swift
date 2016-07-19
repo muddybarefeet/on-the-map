dab546505cde94b172d4f541ce5d3d8da4eafcfb
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-19 07:06:28 -0700

    work on editing location

--- onTheMap/LocationEditorViewController.swift
+++ onTheMap/LocationEditorViewController.swift
@@ -8,17 +8,65 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 class LocationEditorViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var answerText: UITextView!
+    @IBOutlet weak var answerText: UITextField!
+    @IBOutlet weak var locationView: MKMapView!
+    @IBOutlet weak var submitButton: UIButton!
     
-    let textDelegate = AnswerTextViewDelegate()
+    let TextDelegate = TextFieldDelegate()
+//    let MapDelegate = MapViewDelegate()
     
     override func viewDidLoad() {
+        submitButton.enabled = false
         answerText.textAlignment = .Center
-        answerText.delegate = textDelegate
+        answerText.delegate = TextDelegate
+        submitButton.layer.cornerRadius = 10
+        submitButton.titleEdgeInsets = UIEdgeInsetsMake(10, 10, 10, 10)
     }
+    
+    @IBAction func selectLocation(sender: AnyObject) {
+        print("select location")
+        getLocation(answerText.text!)
+    }
+    
+    //want these to be moved back to the view controller at some point
+    func getLocation (address: String) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address, completionHandler: {(placemarks, error) -> Void in
+            if((error) != nil) {
+                print("Error", error)
+            }
+            if let placemark = placemarks?.first {
+                let coordinates:CLLocationCoordinate2D = placemark.location!.coordinate
+                self.plotLocation(coordinates)
+            }
+        })
+    }
+    
+    func geocodeAddressString(addressString: String, completionHandler: CLGeocodeCompletionHandler) {}
+    
+    func plotLocation(coords: CLLocationCoordinate2D) {
+        //move the map view to the right place
+        let latDelta:CLLocationDegrees = 0.1
+        let longDelta:CLLocationDegrees = 0.1
+        let theSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        let pointLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(coords.latitude, coords.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(pointLocation, theSpan)
+        locationView.setRegion(region, animated: true)
+        
+        //drop the pin in the correct location
+        let dropPin = MKPointAnnotation()
+        dropPin.coordinate = coords
+        dropPin.title = self.answerText.text
+        self.locationView.addAnnotation(dropPin)
+        //update the text in the button to confirm View
+        submitButton.setTitle("Confirm Location", forState: .Normal)
+        submitButton.sizeToFit()
+    }
+    
     
 }
---
+++ onTheMap/TextFieldDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  TextFieldDelegate.swift
+//  onTheMap
+//
+//  Created by Anna Rogers on 7/19/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+class TextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        textField.text = ""
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        var newText: NSString
+        newText = textField.text!
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("enter hit!")
+        textField.resignFirstResponder()
+        return true
+    }
+    
+
+    
+}
