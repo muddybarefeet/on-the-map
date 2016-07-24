64504f557bcb00a849760c5c7e65b8f410eced60
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-24 06:20:37 -0700

    add constants file

---
+++ onTheMap/Constants.swift
@@ -0,0 +1,58 @@
+//
+//  Constants.swift
+//  onTheMap
+//
+//  Created by Anna Rogers on 7/19/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Constants {
+    
+    struct Parse {
+        static let baseURL = "https://api.parse.com/1/classes/"
+        static let StudentLocation = "StudentLocation"
+    }
+    
+    struct Udacity {
+        static let baseURL = "https://www.udacity.com/api/"
+        static let Session = "session"
+        static let Users = "users/"
+        static let UdacitySignUp = "https://www.udacity.com/account/auth#!/signup"
+    }
+    
+    struct ParseParameterKeys {
+        static let ApiKey = "api_key"
+        static let Order = "order"
+        static let ApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let ApplicationKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let Where = "where"
+    }
+
+    
+    struct UdacityParameterKeys {
+        static let JSONField = "application/json"
+    }
+    
+    
+    struct UdacityParameterValues {
+        static let AcceptJSON = "Accept"
+        static let ContentType = "Content-Type"
+    }
+    
+    // MARK: TMDB Parameter Values
+    struct ParseParameterValues {
+        static let ApplicationID = "X-Parse-Application-Id"
+        static let ApplicationKey = "X-Parse-REST-API-Key"
+        static let UpdatedAt = "-updatedAt"
+    }
+    
+    // MARK: TMDB Response Keys
+    struct ParseResponseKeys {
+        static let Title = "title"
+        static let ID = "id"
+    }
+    
+}
